f8cb9c7ae732a5ae5af738308c91a5486d70ffc3
Chwiggy <emiilycwilke@gmail.com>
2024-04-06 19:32:44 +0200

    TODOs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// TODO Reading in GTFS File
+// TODO option: clipping by bounds
+// TODO option: filtering by various categories
+// TODO option: clipping by time
