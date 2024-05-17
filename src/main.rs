e11a696df67dd16eeeab8fb1f1f2da5fea6d927a
Chwiggy <emilycwilke@gmail.com>
2024-05-17 14:43:57 +0200

    module split bug fixes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod gtfs;
 
 fn main() {
-    let gtfs_path: String = String::from("/home/lotte/code/github.com/chwiggy/gtfs-tooling/test_data/20240513_fahrplaene_gesamtdeutschland_gtfs.zip");
+    // TODO add user input instead of hard coding.
+    let gtfs_path: String = String::from("test_data/20240513_fahrplaene_gesamtdeutschland_gtfs.zip");
 
-    let gtfs_file: modname::GtfsFile = modname::GtfsFile::new(&gtfs_path);
+    let gtfs_file: gtfs::GtfsFile = gtfs::GtfsFile::new(&gtfs_path);
     let file_list: Vec<String> = gtfs_file.list_files();
 
     println!("GTFS archive contains: {:?}", file_list);
