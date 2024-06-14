d5bb28ccfb2a2c9709efcb2402b22bde49759091
Chwiggy <emilycwilke@gmail.com>
2024-06-14 12:14:31 +0200

    added clap subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,85 @@
 mod gtfs;
-use clap::Parser;
+use clap::{Parser, Subcommand, Args};
 use crate::gtfs::{GtfsObject, CalendarDates};
 
 #[derive(Parser)]
+#[command(version, about, long_about = None)]
 struct Cli {
-    pattern:String,
-    path: std::path::PathBuf
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Echo(EchoArgs),
+}
+
+#[derive(Args)]
+struct EchoArgs {
+    input: std::path::PathBuf,
+    
+    #[command(subcommand)]
+    file: StandardFiles,
+}
+
+#[derive(Subcommand)]
+enum StandardFiles {
+    Files,
+    CalendarDates
 }
 
 fn main() {
     // TODO add user input instead of hard coding.
     let args = Cli::parse();
 
-    let gtfs_path = args.path;
+    match args.command {
+        Commands::Echo(args) => {
+            let gtfs_path = args.input;
+            match args.file {
+                StandardFiles::Files => {
+                    file_list(gtfs_path)
+                },
+                StandardFiles::CalendarDates => {
+                    calendar_dates_out(gtfs_path)
+                },
 
-    if args.pattern == String::from("echo") {
-        let mut gtfs_file = match gtfs::GtfsFile::new(&gtfs_path) {
-            Ok(gtfs_file) =>  gtfs_file,
-            Err(error) => panic!("{}", error)
-        };
-    
-        
-        let file_list: Vec<String> = gtfs_file.list_files();
-    
-        println!("GTFS archive contains: {:?}", file_list);
-    
-        // let agencies: Vec<Agency> = Agency::from_gtfs_file(&mut gtfs_file);
-    
-    
-        let stop: Vec<CalendarDates> = CalendarDates::from_gtfs_file(&mut gtfs_file);
-        println!("{:?}",stop)
+            }
+            
+        }
     }
     
 }
+
+fn load_gtfs_file(gtfs_path: std::path::PathBuf) -> gtfs::GtfsFile {
+    let gtfs_file = match gtfs::GtfsFile::new(&gtfs_path) {
+        Ok(gtfs_file) =>  gtfs_file,
+        Err(error) => panic!("{}", error)
+    };
+    gtfs_file
+}    
+
+fn file_list(gtfs_path: std::path::PathBuf) {
+    let mut gtfs_file = load_gtfs_file(gtfs_path);
+
+    let file_list: Vec<String> = gtfs_file.list_files();
+        
+    println!("GTFS archive contains: {:?}", file_list);
+}
+
+fn calendar_dates_out(gtfs_path: std::path::PathBuf) {
+    let mut gtfs_file = load_gtfs_file(gtfs_path);
+               
+    // let agencies: Vec<Agency> = Agency::from_gtfs_file(&mut gtfs_file);
+    
+    
+    let stop: Vec<CalendarDates> = CalendarDates::from_gtfs_file(&mut gtfs_file);
+    println!("{:?}",stop)
+}
+
+
+
+
+
 
 
 // TODO Reading in GTFS File
