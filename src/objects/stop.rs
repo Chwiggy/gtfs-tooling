c44facc64965d7f653d37575ab7bc5c9f200c9a9
Chwiggy <emilycwilke@gmail.com>
2024-09-20 21:27:17 +0200

    feat: refactor to use BtreeMaps, disentangled logic

--- src/objects/stop.rs
+++ src/objects/stop.rs
@@ -1,50 +1,13 @@
-use crate::functions::gtfs::{self, Stop, StopTime};
+use crate::functions::gtfs::{self, Stop, StopTime, Trip};
 use std::collections::BTreeMap;
 
 
 pub struct FullStop {
     stop_description: Stop,
-    stop_times: Option<Vec<StopTime>>,
-    associated_trips: Option<Vec<String>>
+    stop_times: Vec<StopTime>,
+    associated_trips: Vec<Trip>
 }
 
-impl FullStop {
-    fn from_stop(stop: &Stop) -> Self {
-        FullStop{
-            stop_description: stop.clone(),
-            stop_times: None,
-            associated_trips: None,
-        }
-    }
-
-    fn add_stop_times(mut self, gtfs_file: &mut gtfs::GtfsFile) -> Self {
-        let stop_times: Vec<gtfs::StopTime> = gtfs_file.read_vec();
-        let mut local_stop_vec: Vec<StopTime> = Vec::new();
-        
-        for stop_time in stop_times {
-            if let Some(id) = stop_time.stop_id.clone() {
-                if id == self.stop_description.stop_id{
-                    local_stop_vec.push(stop_time)
-                }
-            }
-            
-        }
-
-        self.stop_times = Some(local_stop_vec);
-        self
-    }
-
-    fn add_associated_trips(mut self) -> Self {
-        if let Some(stop_times) = &self.stop_times {
-            let mut trips_vec: Vec<String> = Vec::new();
-            for time in stop_times {
-                trips_vec.push(time.trip_id.to_string());
-            }
-            self.associated_trips = Some(trips_vec);
-        } 
-        self
-    }
-}
 
 
 pub fn print_stop_details(stop_id: String, gtfs_file: &mut gtfs::GtfsFile) {
@@ -53,26 +16,65 @@ pub fn print_stop_details(stop_id: String, gtfs_file: &mut gtfs::GtfsFile) {
         None => println!("Stop id {} not found", stop_id),
         Some(stop) => {
             println!("{:?}", stop.stop_description);
-            if let Some(trips) = stop.associated_trips {
-                for id in trips {
-                    println!("{}", id);
-                }
-            }
+            println!("{:?}", stop.stop_times);
+            println!("{:?}", stop.associated_trips);
         }
     }   
 }
 
 fn extract_stop_info(gtfs_file: &mut gtfs::GtfsFile, id: &String) -> Option<FullStop> {
+    
+    let stop = get_stop_description(gtfs_file, id)?;
+    let stop_times = get_stop_times(gtfs_file, id);
+    let associated_trips = get_matching_trips(gtfs_file, &stop_times);
+
+    
+    let stop_info = FullStop {
+        stop_description: stop,
+        stop_times: stop_times,
+        associated_trips: associated_trips,
+    };
+
+    Some(stop_info)
+}
+
+fn get_stop_description(gtfs_file: &mut gtfs::GtfsFile, id: &String) -> Option<Stop> {
     let stops: Vec<gtfs::Stop> = gtfs_file.read_vec();
-
     let stops_map = stops_hash(stops);
-
     let stop = stops_map.get(id)?;
+    Some(stop.clone())
+}
 
-    let mut full_stop = FullStop::from_stop(stop);
-    full_stop = full_stop.add_stop_times(gtfs_file);
-    full_stop = full_stop.add_associated_trips();
-    Some(full_stop)
+fn get_stop_times(gtfs_file: &mut gtfs::GtfsFile, stop_id: &String) -> Vec<StopTime> {
+    let stop_times: Vec<gtfs::StopTime> = gtfs_file.read_vec();
+    let mut local_stop_vec: Vec<StopTime> = Vec::new();
+    
+    for stop_time in stop_times {
+        if let Some(id) = stop_time.stop_id.clone() {
+            if id == stop_id.to_owned() {
+                local_stop_vec.push(stop_time)
+            }
+        }
+        
+    }
+
+    local_stop_vec
+}
+
+fn get_matching_trips(gtfs_file: &mut gtfs::GtfsFile, stop_times: &Vec<StopTime>) -> Vec<Trip> {
+    let trips: Vec<gtfs::Trip> = gtfs_file.read_vec();
+    let mut trip_map: BTreeMap<String, Trip> = BTreeMap::new();
+    for trip in trips {
+        trip_map.insert(trip.trip_id.clone(), trip);
+    }
+
+    let mut matching_trips: Vec<Trip> = Vec::new();
+    for stop_time in stop_times {
+        let matching_trip = trip_map.remove(&stop_time.trip_id).unwrap();
+        matching_trips.push(matching_trip);
+    }
+
+    matching_trips
 }
 
 fn stops_hash(stops: Vec<Stop>) -> BTreeMap<String, Stop> {
